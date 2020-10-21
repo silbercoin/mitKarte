98f1a20f6d7e63d3d71cf8b325a9b88e70f66c75
juan <wanjeng@code.berlin>
2020-10-21 14:26:14 +0200

    renew landing page

---
+++ form.js
@@ -0,0 +1,14 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBQ7IYx1KbPuCQP3aM9_TLgo6LRKQbd1gg",
+    authDomain: "mitkarte-d3451.firebaseapp.com",
+    databaseURL: "https://mitkarte-d3451.firebaseio.com",
+    projectId: "mitkarte-d3451",
+    storageBucket: "mitkarte-d3451.appspot.com",
+    messagingSenderId: "613908235620",
+    appId: "1:613908235620:web:ac2a384cb888a26776e671"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
